5aee18dfa43c64ef69c160e3059c52d67fd352bb
Jingwei Zhu <jingweizhucn@126.com>
2020-07-27 10:28:53 +0800

    update

--- start-container.sh
+++ start-container.sh
@@ -1,4 +1,5 @@
-user_id=$(id -u ${USER})
-group_id=$(id -g ${USER})
-docker-compose build --build-arg USER_ID=$user_id --build-arg GROUP_ID=$group_id
-docker-compose up -d 
+#!/bin/bash
+export user_id=$(id -u ${USER}) 
+export group_id=$(id -g ${USER}) 
+docker-compose up -d --build
+#docker-compose build --build-arg USER_ID=$user_id --build-arg GROUP_ID=$group_id
