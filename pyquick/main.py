f2dd16342f8f9e087cc0a8996c35e49a61b98908
Jingwei Zhu <jingweizhucn@126.com>
2020-08-06 16:49:17 +0800

    update

--- pyquick/main.py
+++ pyquick/main.py
@@ -5,8 +5,11 @@ def arg_parser():
     parser.add_argument('--version', action='version', version='quick 0.0.1')
     return parser
 
+def hi(a,b,/,c):
+    print(a,b,c)
+
 def main():
-    print('I like 955!')
+    hi('I', 'Like', c='955')
     args = arg_parser().parse_args()
 
 if __name__ == '__main__':
